e38160c68f53db3ca589f0f410c402705257d1c1
priyabarnwal <priya.barnwal@gmail.com>
2025-06-24 22:23:33 +0530

    Scroll and style fixes

--- components/chatbot.tsx
+++ components/chatbot.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FormEvent, useState } from "react";
+import { FormEvent, useState, useRef, useEffect } from "react";
 import { RiChatSmile2Fill } from "react-icons/ri"
 import BotMessage from "./ui/botMessage";
 import UserMessage from "./ui/userMessage";
@@ -10,12 +10,23 @@ import { Message } from "@/types";
 
 
 export default function Chatbot() {
+  const chatbotRef = useRef<HTMLDivElement>(null)
+  
   const [showChatbot, setShowChatbot] = useState<boolean>(false)
   const [inputValue, setInputValue] = useState<string>("")
   const [messages, setMessages] = useState<Message[]>([
     { role: "assistant", content: "Hello, I am Chatty! How can I assist you today?" }
   ])
   const [loading, setLoading] = useState<boolean>(false)
+  
+  useEffect(() => {
+    if(chatbotRef.current) {
+      chatbotRef.current.scrollTo({
+        top: chatbotRef.current.scrollHeight,
+        behavior: "smooth"
+      })
+    }
+  }, [ messages, loading])
 
   const handleSend = async(e: FormEvent) => {
     e.preventDefault()
@@ -42,12 +53,12 @@ export default function Chatbot() {
     <div>
         <RiChatSmile2Fill size={42} className="fixed right-12 bottom-[calc(1rem)] hover:cursor-pointer" onClick={() => setShowChatbot(!showChatbot)}/>
         {showChatbot && (
-        <div className="fixed right-12 bottom-[calc(4.5rem)] p-3 shadow-sm shadow-white border rounded-md w-105 h-150   bg-gray-900">
-            <div className="flex flex-col items-center">
-                <h2 className="text-xl font-bold mt-1">Chatty</h2>
-                <p className="text-xs">( powered by OpenAI )</p>
+        <div className="fixed right-12 bottom-[calc(4.5rem)] p-3 shadow-sm shadow-white border rounded-md w-105 h-155   bg-gray-900">
+            <div className="flex flex-col items-center border-b border-white pb-1">
+                <h2 className="text-xl font-bold">Chatty</h2>
+                <p className="text-xs mb-1">( powered by OpenAI )</p>
             </div>
-            <div className="flex flex-col h-[79%] overflow-y-auto mt-4 p-2">
+            <div ref={chatbotRef} className="flex flex-col h-[79%] overflow-y-auto mt-4 p-2">
               {messages?.map((message, index) => {
                 if (message.role === "assistant") {
                   return <BotMessage key={index} message={message.content} />
